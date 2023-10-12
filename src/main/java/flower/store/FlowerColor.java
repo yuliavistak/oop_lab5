19c70561f740ba7c71edd7db0e298f5d0004bba6
yuliavistak <vistak.pn@ucu.edu.ua>
2023-10-12 21:16:42 +0300

    Added first files woth implemetation

---
+++ src/main/java/flower/store/FlowerColor.java
@@ -0,0 +1,19 @@
+package flower.store;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter@ToString
+public enum FlowerColor {
+    RED("#FF0000"), GREEN("#32A852"), YELLOW("#F2DD1B");
+    String colorCode;
+
+    FlowerColor (String colorCode){
+        this.colorCode = colorCode;
+    }
+
+    public boolean hasThisColor(String otherColor) {
+        return colorCode.equals(otherColor);
+    }
+}
