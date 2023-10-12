e858b8d90ec1ffb296f1f394f9637f09c3c62274
Yuliia <116526148+yuliavistak@users.noreply.github.com>
2023-10-12 23:58:01 +0300

    Update Store.java

--- src/main/java/flower/store/Store.java
+++ src/main/java/flower/store/Store.java
@@ -9,10 +9,10 @@ public class Store {
 
     private List<Item> items;
 
-    public List<Item> search(SearchFilter filter){
-        List<Item> foundItems = new ArrayList<> ();
-        for (Item item: items){
-            if (filter.match(item)){
+    public List<Item> search(SearchFilter filter) {
+        List<Item> foundItems = new ArrayList<>();
+        for (Item item: items) {
+            if (filter.match(item)) {
                 foundItems.add(item);
             }
         }
