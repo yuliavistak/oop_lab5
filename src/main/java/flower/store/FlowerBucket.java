369a0207e618e7915a48df8f7d2b5173ba36a341
yuliavistak <vistak.pn@ucu.edu.ua>
2023-10-12 21:35:33 +0300

    Added files with rest of implementation

---
+++ src/main/java/flower/store/FlowerBucket.java
@@ -0,0 +1,24 @@
+package flower.store;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FlowerBucket extends Item{
+    private List<FlowerPack> flowerPacks = new ArrayList<>();
+
+    public double getPrice(){
+        double price = 0;
+        for (FlowerPack flowerPack: flowerPacks) {
+            price += flowerPack.getPrice();
+        }
+        return price;
+    }
+
+    public void add(FlowerPack flowerPack){
+        flowerPacks.add(flowerPack);
+    }
+
+    public List<FlowerPack> getPacks() {
+        return this.flowerPacks;
+    }
+}
