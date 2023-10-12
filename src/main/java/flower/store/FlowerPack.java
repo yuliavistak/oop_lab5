33b2b4aea72ceb4c76c2987ec939b930d0fe292a
Yuliia <116526148+yuliavistak@users.noreply.github.com>
2023-10-12 23:56:34 +0300

    Update FlowerPack.java

--- src/main/java/flower/store/FlowerPack.java
+++ src/main/java/flower/store/FlowerPack.java
@@ -7,19 +7,23 @@ public class FlowerPack {
     private Flower flower;
     private int quantity;
 
-    public FlowerPack (Flower flower, int quantity){
+    public FlowerPack(Flower flower, int quantity) {
         this.flower = new Flower(flower);
         this.quantity = quantity;
     }
 
-    public void setQuantity(int quantity){
-        this.quantity = quantity < 1 ? 1: quantity;
+    public void setQuantity(int quantity) {
+        if(quantity < 1){
+            this.quantity = 1;
+        }
+        else{
+            this.quantity = quantity;
     }
 
-    public double getPrice(){
+    public double getPrice() {
         return quantity * flower.getPrice();
     }
-    public Flower getFlower(){
+    public Flower getFlower() {
         return this.flower;
     }
 }
